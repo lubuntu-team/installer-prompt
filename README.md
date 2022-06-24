76bd63729ce4e4e0e5976c697515ec9d88f08c90
Simon Quigley <simon@tsimonq2.net>
2022-06-23 23:25:15 -0500

    Initial mockup. Very very ugly. Needs tons of cleanup.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lubuntu Installer Prompt
+
+This project is in **VERY VERY EARLY ALPHA**. It doesn't even do anything yet.
+
+Eventually it's going to present a "Try or Install Lubuntu" menu.
+
+Licensing info: 2022 Lubuntu Developers <lubuntu-devel@lists.ubuntu.com>
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,13 +1,24 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include <QScreen>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    move(screen()->geometry().center() - frameGeometry().center());
+
+    // Set the background image, and let it change with the release
+    QPixmap bg("/usr/share/lubuntu/wallpapers/lubuntu-default-wallpaper.png");
+    bg = bg.scaled(this->size(), Qt::IgnoreAspectRatio);
+    QPalette palette;
+    palette.setBrush(QPalette::Window, bg);
+    this->setPalette(palette);
+
+    // Set the top label as the Lubuntu Logo
+    QPixmap logo("../img/logo.png");
+    int w = ui->logolabel->width();
+    int h = ui->logolabel->height();
+    ui->logolabel->setPixmap(logo.scaled (w,h,Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
