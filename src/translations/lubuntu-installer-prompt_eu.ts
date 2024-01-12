c627f5d3400a42c2747242ac7c31de49f5f70ba0
roberalz <roberalz@lubuntu.me>
2024-01-12 13:53:51 +0000

    updated Basque translation

--- src/translations/lubuntu-installer-prompt_eu.ts
+++ src/translations/lubuntu-installer-prompt_eu.ts
@@ -6,141 +6,141 @@
     <message>
         <location filename="../installerprompt.ui" line="20"/>
         <source>Try or Install Lubuntu</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Saiatu edo instalatu Lubuntu</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="237"/>
         <source>Select Your Language:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Hautatu zure hizkuntza:</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="263"/>
         <source>✅ Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">✅ Baieztatu</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="280"/>
         <source>You entered an incorrect password. Please try again.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Pasahitz oker bat sartu duzu. Saiatu berriro mesedez.</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="320"/>
         <source>Select a Wi-Fi Network:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Hautatu Wi-Fi sare bat:</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="380"/>
         <source>(For advanced network configuration, select &quot;Try Lubuntu&quot;)</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">(Sarearen konfigurazio aurreratuetarako, hautatu &quot;Probatu Lubuntu&quot;)</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="419"/>
         <source>✅ Connect</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">✅ Konektatu</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="433"/>
         <location filename="../installerprompt.cpp" line="475"/>
         <source>Changing language...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Hizkuntza aldatzen...</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="525"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Try Lubuntu without installing it on your system. Allows you to use programs, browse the Web, and even temporarily install apps without modifying your system. Changes made within the live environment will be lost when the computer shuts down.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Saiatu Lubuntu zure sisteman instalatu gabe. Programak erabiltzeko, Weba arakatzeko eta baita aplikazioak aldi baterako instalatzeko ere zure sistema aldatu gabe. Zuzeneko ingurunean egindako aldaketak ordenagailua itzaltzean galduko dira.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="539"/>
         <source>  Try Lubuntu</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">  Saiatu Lubuntu</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="589"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Install Lubuntu onto your system. You can install Lubuntu by itself, or alongside an existing operating system.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-size:12pt;&quot;&gt;Instalatu Lubuntu zure sisteman. Lubuntu bakarrik instala dezakezu, edo lehendik dagoen sistema eragile batekin batera.&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="600"/>
         <source>  Install Lubuntu</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">  Instalatu Lubuntu</translation>
     </message>
     <message>
         <location filename="../installerprompt.ui" line="727"/>
         <location filename="../installerprompt.cpp" line="114"/>
         <source>Connected</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Konektatuta</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="49"/>
         <location filename="../installerprompt.cpp" line="256"/>
         <location filename="../installerprompt.cpp" line="271"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Errorea</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="49"/>
         <source>Background image cannot be loaded.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ezin da atzeko planoko irudia kargatu.</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="108"/>
         <source>Not Connected</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ez dago konektatuta</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="119"/>
         <source>Connecting...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Konektatzen...</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="124"/>
         <source>Disconnecting...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Deskonektatzen...</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="130"/>
         <source>Unknown Status</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Egoera ezezaguna</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="194"/>
         <source>Wi-Fi Password Required</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Wi-Fi pasahitza beharrezkoa da</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="203"/>
         <source>Enter password for &quot;%1&quot;:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Sartu &quot;%1&quot;-ren pasahitza:</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="205"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Konektatu</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="256"/>
         <source>Failed to create Wi-Fi settings.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ezin izan dira sortu Wi-Fi ezarpenak.</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="271"/>
         <source>Failed to add Wi-Fi connection.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ezin izan da gehitu Wi-Fi konexioa.</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="330"/>
         <source>WiFi Not Available</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">WiFi Ez dago erabilgarri</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="330"/>
         <source>No WiFi devices were found on this system.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ez da WiFi gailurik aurkitu sistema honetan.</translation>
     </message>
     <message>
         <location filename="../installerprompt.cpp" line="472"/>
         <source>Unable to download full language support, changing anyway...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ezin da hizkuntza osoa deskargatu, hala ere aldatzen ari...</translation>
     </message>
 </context>
 </TS>
